05020aa7999f7b41ec0c6ce5c3f2f612b5b8fcdb
gijs.fiten <gijs.fiten@student.kuleuven.be>
2021-04-29 17:33:16 +0200

    ignore this lol

--- Creators/RecursiveBacktrackGenerator.cpp
+++ Creators/RecursiveBacktrackGenerator.cpp
@@ -95,6 +95,5 @@ void recursiveBacktrack(int x, int y, int WIDTH, int HEIGHT){
             recursiveBacktrack(i % WIDTH, i / WIDTH, WIDTH, HEIGHT);
         }
     }
-    neighbours.clear();
     return;
 }
